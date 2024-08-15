167a0f038b520357f01649597f4598416e2e1f0d
Rahulsingh1810 <rahul181002singh@gmail.com>
2024-08-15 13:45:47 +0530

    connection error with mongoDB

---
+++ server/config/connectDB.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+async function connectDB(){
+    try{
+            await mongoose.connect(process.env.MongoDB_URI)
+
+            const connection = mongoose.connection
+
+            connection.on('connected', ()=>{
+                console.log("connected to DB")
+            })
+
+            connection.on('error', (error)=> {
+                console.log("Something is wrong in mongoDB", error)
+            })
+    } catch (error){
+        console.log("something is wrong", error)
+    }
+}
+
+module.exports = connectDB
