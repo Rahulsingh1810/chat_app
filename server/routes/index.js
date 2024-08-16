5252c99891bfaed161e83b83f52867613dbdf0b7
Rahulsingh1810 <rahul181002singh@gmail.com>
2024-08-16 15:14:38 +0530

    all the api and the backend part is done

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const checkEmail = require('../controller/checkEmail')
 const checkPassword = require ('../controller/checkPassword')
 const userDetails = require('../controller/useDetails')
+const logout = require('../controller/logout')
+const updateUserDetails = require('../controller/updateUserDetails')
 
 //create user api
 router.post('/register',registerUser)
@@ -19,6 +21,14 @@ router.post('/password',checkPassword)
 //login user details api
 
 router.get('/user-details',userDetails)
+
+//logout user api
+
+router.get('/logout',logout)
+
+//update user api
+
+router.post('/update-user',updateUserDetails)
 
 
 module.exports = router
