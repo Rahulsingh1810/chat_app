fabd330376469f50b1c1ee9b0a291d3301bf0b57
Rahulsingh1810 <rahul181002singh@gmail.com>
2024-08-16 11:47:02 +0530

    issues with user details api

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const registerUser = require('../controller/registerUser')
 const router = express.Router()
 const checkEmail = require('../controller/checkEmail')
 const checkPassword = require ('../controller/checkPassword')
+const userDetails = require('../controller/useDetails')
 
 //create user api
 router.post('/register',registerUser)
@@ -14,6 +15,10 @@ router.post('/email',checkEmail)
 //check password api
 
 router.post('/password',checkPassword)
+
+//login user details api
+
+router.get('/user-details',userDetails)
 
 
 module.exports = router
