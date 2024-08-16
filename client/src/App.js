09aeb388d5427f831223997dd6ea337cee8d0e9d
Rahulsingh1810 <rahul181002singh@gmail.com>
2024-08-16 20:12:25 +0530

    started with frontend and done the routing

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+   <main className=''>
+      <Outlet/>
+
+   </main>
+  );
+}
+
+export default App;
